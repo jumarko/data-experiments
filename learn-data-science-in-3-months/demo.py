57e518ebb781ad08bef6dbefa182e67d3ee3c829
Juraj Martinka <jumarko@gmail.com>
2020-03-02 14:17:06 +0100

    Learn Data Science in 3 months: Python intro - first video + 2 classifiers

--- learn-data-science-in-3-months/demo.py
+++ learn-data-science-in-3-months/demo.py
@@ -1,4 +1,8 @@
-from sklearn import tree
+# see also https://scikit-learn.org/stable/getting_started.html
+# https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 
 # use specific submodule to build a decision tree
 X = [[181,80,44], [177,70,43], [160,60,38], 
@@ -9,9 +13,11 @@ Y = ['male', 'female', 'female',
      'female', 'male', 'male', 
      'male', 'female', 'male']
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X,Y)
+def fitAndReport(classifier):
+    clf = classifier().fit(X,Y)
+    prediction = clf.predict([[190,70,43]])
+    print("Using classifier", classifier, ": \n  result=", prediction)
 
-prediction = clf.predict([[190,70,43]])
-
-print("Prection: ", prediction)
+fitAndReport(DecisionTreeClassifier)
+fitAndReport(RandomForestClassifier)
+fitAndReport(GaussianNB)
